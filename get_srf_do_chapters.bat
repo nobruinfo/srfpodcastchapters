d3aca4d0f09c28e748967f0eeb11a5055ef7fce0
nobruinfo <67829169+nobruinfo@users.noreply.github.com>
2021-03-14 17:51:19 +0100

    Cleanup, check bracket for timestamp
    In this version a fix had to ensure lines of the pod description begins with an opening bracket. Before additional texts such as links to this site here lead to generation of fake chapters. One could say a trap of its own success.
    Many lines of the initial script were deleted since they were left from a template script of a completely different origin.

--- get_srf_do_chapters.bat
+++ get_srf_do_chapters.bat
@@ -1,41 +1,13 @@
 @ECHO OFF
 REM ------------------ Begin of section to fill in -----------------------------
 SET nir="D:\Eigene Programme\Nircmd\nircmd.exe"
-SET zenitypath=D:\Eigene Programme\Tools\Batch\zenity-2.28.0_win32-3\bin
-SET zenity="%zenitypath%\zenity.exe"
-SET ZENITY_DATADIR=D:\Eigene Programme\Tools\Batch\zenity-2.28.0_win32-3\share\
-SET x="D:\Eigene Programme\Internet\MediathekView_6\bin\ffmpeg.exe"
-SET x="D:\Eigene Programme\Grafik\FFMPEG\ffmpeg-3.2.4-win64-static\bin\ffmpeg.exe"
 SET x="D:\Eigene Programme\Tools\Batch\FFMPEG\youtube-dl.exe"
-SET optsnodate=--download-archive archiv.ini --max-downloads 100 --cookies cookies.txt
+SET xx="D:\Eigene Programme\Internet\MediathekView_6\bin\ffmpeg.exe"
+SET mi="D:\Eigene Programme\Grafik\MediaInfo_CLI_0.7.72_Windows_x64\MediaInfo.exe"
+SET optsnodate=--download-archive archiv.ini --max-downloads 10 --cookies cookies.txt
 SET opts=%optsnodate% --dateafter 20200505
 SET optsnodate=%optsnodate% --exec "COPY /b {} +,,"
 REM ------------------- End of section to fill in ------------------------------
-
-REM --------------------- Begin of documentation -------------------------------
-REM Zwischendurch mit  youtube-dl.exe -U
-REM überprüfen, ob es Updates gibt.
-REM
-REM Quelle: https://github.com/ytdl-org/youtube-dl
-REM RSS: https://github.com/ytdl-org/youtube-dl/issues/667
-REM
-REM Frühere RSS von Youtube waren so verwendet worden:
-REM https://www.youtube.com/feeds/videos.xml?channel_id=UCMQ3E2ZfpvnUkpUen7iMwHQ
-REM Jetzt nur noch als Playlist oder die Liste aller Videos.
-REM 
-REM Beschränkung gegen ganz alte Videos oben bei den Optionen zu setzen.
-REM 
-REM CC2tv hätte noch diese eigene RSS-Liste: http://cc2.tv/feedv.xml
-REM Wird nicht verwendet, da er ja eine Playlist für die Gesamt-Episoden unter-
-REM hält.
-REM 
-REM 
-REM
-REM 
-REM 
-REM 
-REM 
-REM ---------------------- End of documentation --------------------------------
 
 %nir% win setsize foreground 5 5 500 500
 mode 140,80
@@ -55,34 +27,11 @@ chcp 1252
 REM This to have those !vars! at hand which aren't preset outside loops:
 setlocal enabledelayedexpansion
 
-IF EXIST "D:\Eigene Programme\Tools\Batch\FFMPEG\*.*" (
-  SET DRIVE=D:
-) ELSE (
-  SET DRIVE=C:
-)
-SET PATH=%PATH%;%DRIVE%\Eigene Programme\Grafik\Latex\Python27
-SET PATH=%PATH%;%DRIVE%\Eigene Programme\Grafik\Latex\Python27\Lib\site-packages
-SET PATH=%PATH%;%DRIVE%\Eigene Programme\Grafik\FFMPEG\ffmpeg-3.2.4-win64-static\bin
-
-
-REM Now remove all paths that could interfere:
-CALL SET PATH=%%PATH:C:\Users\Superuser\AppData\Local\Microsoft\WindowsApps=%%
-CALL SET PATH=%%PATH:C:\Program Files (x86)\GnuPG\bin=%%
-CALL SET PATH=%%PATH:C:\Program Files ^(x86^)\Common Files\Oracle\Java\javapath=%%
-CALL SET PATH=%%PATH:C:\ProgramData\Oracle\Java\javapath=%%
-CALL SET PATH=%%PATH:C:\Windows\System32\OpenSSH\=%%
-CALL SET PATH=%%PATH:C:\tools\Cmder=%%
-CALL SET PATH=%%PATH:C:\ProgramData\chocolatey\bin=%%
-CALL SET PATH=%%PATH:;;=;%%
-
-REM ------ TO BE REMOVED ------
-CD /D W:\aaaaa
+REM If you want to directly save downloades podcast files to somewhere else:
+REM CD /D W:\aaaaa
 
 TITLE SRF Digital:
 %x% %opts% --write-description http://podcasts.srf.ch/digital_plus_mpx.xml
-
-SET xx="D:\Eigene Programme\Internet\MediathekView_6\bin\ffmpeg.exe"
-SET mi="D:\Eigene Programme\Grafik\MediaInfo_CLI_0.7.72_Windows_x64\MediaInfo.exe"
 
 REM Loop all description files:
 FOR /R . %%F in (*.description) do (
@@ -113,31 +62,34 @@ FOR /R . %%F in (*.description) do (
       IF !sum! GEQ 3 (
         echo line!sum! = %%A
         SET aa=%%A
+        SET chk=!aa:~0,1!
         
-        SET "ft=!aa:(=!"
-        SET "ddd=!ft:)=!"
-        REM SET beg=!ddd:~0,8!.!ddd:~9,2!.!ddd:~4,4!
-        REM SET beg=!ddd:~0,8!!
-        SET hh=!ddd:~0,2!!
-        SET min=!ddd:~3,2!!
-        SET sec=!ddd:~6,2!!
-        REM Remove leading zeroes:
-        set /a hh=10000!hh! %% 10000
-        set /a min=10000!min! %% 10000
-        set /a sec=10000!sec! %% 10000
-        ECHO hh=!hh! min=!min! sec=!sec!
-        SET /a end=!hh!*3600+!min!*60+sec-1
-        ECHO Formula done
-        SET end=!end!000
-        SET txt=!ddd:~9,99!!
-        ECHO END=!end!>>!NAME!.meta
-        ECHO title=!lasttitle!>>!NAME!.meta
-        SET lasttitle=!txt!
-  
-        SET /a beg=!end!+1
-        ECHO [CHAPTER]>>!NAME!.meta
-        ECHO TIMEBASE=1/1000>>!NAME!.meta
-        ECHO START=!beg!>>!NAME!.meta
+        IF "!chk!" == "(" (
+          SET "ft=!aa:(=!"
+          SET "ddd=!ft:)=!"
+          REM SET beg=!ddd:~0,8!.!ddd:~9,2!.!ddd:~4,4!
+          REM SET beg=!ddd:~0,8!!
+          SET hh=!ddd:~0,2!!
+          SET min=!ddd:~3,2!!
+          SET sec=!ddd:~6,2!!
+          REM Remove leading zeroes:
+          set /a hh=10000!hh! %% 10000
+          set /a min=10000!min! %% 10000
+          set /a sec=10000!sec! %% 10000
+          ECHO hh=!hh! min=!min! sec=!sec!
+          SET /a end=!hh!*3600+!min!*60+sec-1
+          ECHO Formula done
+          SET end=!end!000
+          SET txt=!ddd:~9,99!!
+          ECHO END=!end!>>!NAME!.meta
+          ECHO title=!lasttitle!>>!NAME!.meta
+          SET lasttitle=!txt!
+    
+          SET /a beg=!end!+1
+          ECHO [CHAPTER]>>!NAME!.meta
+          ECHO TIMEBASE=1/1000>>!NAME!.meta
+          ECHO START=!beg!>>!NAME!.meta
+        )
       )
     )
     !mi! --Inform="General;%%Duration%%" "!NAME!.mp3">arghh.tmp
